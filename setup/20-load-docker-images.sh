15b47a8a72fae410b88f1f8dd90afe714b86e301
Thomas Hunter <th@thomashunter.co.uk>
2019-07-06 19:52:24 +0100

    Increased number of images loaded into kind at once.

--- setup/20-load-docker-images.sh
+++ setup/20-load-docker-images.sh
@@ -3,4 +3,6 @@ source ../common.sh
 
 b_log "Pre-loading required docker images."
 
-cat cached-images | xargs -P2 -n1 -I '{}' sh -c 'echo "Started loading {}"; kind load docker-image {} > /dev/null; echo "Finished loading {}"'
+PARALLEL_JOBS=4
+
+cat cached-images | xargs -P $PARALLEL_JOBS -n1 -I '{}' sh -c 'echo "Started loading {}"; kind load docker-image {} > /dev/null; echo "Finished loading {}"'
