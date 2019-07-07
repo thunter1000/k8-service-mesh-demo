75a1501d2ea56de757e3d4d91edc0d06a1c11603
Thomas Hunter <thunter1000@gmail.com>
2019-07-07 22:05:38 +0100

    Created script for deploying istio to the demo cluster

---
+++ 50-deploy-istio.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+script_dir=$(dirname $0)
+
+source "$script_dir/common.sh"
+
+b_log "Creating 'istio-system' namespace"
+(
+  show_cmds;
+  kubectl create ns istio-system || true
+)
+
+
+b_log "Configure istio service accounts"
+(
+  show_cmds;
+  helm template "$script_dir/_50-deploy-istio/istio-charts/istio-init" --name istio-init --namespace istio-system | kubectl apply -f -
+  kubectl wait --for=condition=complete job -n istio-system --all
+)
+s_log "Istio service accounts created"
+
+
+
+b_log "Deploy istio"
+(
+  show_cmds;
+  helm template "$script_dir/_50-deploy-istio/istio-charts/istio" --name istio --namespace istio-system | kubectl apply -f -
+  kubectl wait --for=condition=available deploy -n istio-system --all
+)
+s_log "Istio has been deployed to the cluster"
