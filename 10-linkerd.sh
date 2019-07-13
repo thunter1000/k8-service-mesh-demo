d735cbc28ac2b62db4508b837857c2d9e744dd4d
Thomas Hunter <thunter1000@gmail.com>
2019-07-13 22:31:28 +0100

    Stop linkerd opening browsers, binding ports and removing itself

--- 10-linkerd.sh
+++ 10-linkerd.sh
@@ -2,15 +2,6 @@
 script_dir=$(dirname $0)
 source "$script_dir/common.sh"
 
-function finish {
-  b_log "Remove emojivoto, linkerd namespaces"
-  kubectl delete namespace emojivoto &
-  kubectl delete namespace linkerd &
-
-  sleep 1
-}
-trap finish EXIT
-
 b_log "Installing linkerd"
 (
   show_cmds;
@@ -33,19 +24,4 @@ b_log "Checking deployment of example application"
 (
   show_cmds;
   linkerd -n emojivoto check --proxy;
-)
-
-b_log "Binding port for example application"
-(
-  show_cmds;
-  kubectl -n emojivoto port-forward svc/web-svc 8080:80 &
-)
-
-b_log "Starting dashboard"
-(
-  show_cmds;
-  linkerd dashboard &
-  show_kube_dashboard &
-  open_web_browser "http://127.0.0.1:8080" &
-  wait;
 )
