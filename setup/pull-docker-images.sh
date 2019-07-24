f6d9ad14f9d4246a1f829d16233ee44d87afa47c
Thomas Hunter <thunter1000@gmail.com>
2019-07-24 08:16:31 +0100

    Remove kube kind scripts
    Recommending Docker Kubernetes.

--- setup/pull-docker-images.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-source ../common.sh
-
-b_log "Pulling docker images for the demo"
-
-cat cached-images | xargs -P10 -n1 -I '{}' sh -c 'echo "Started pulling {}"; docker image pull {} > /dev/null; echo "Finished pulling {}"'
-
-s_log "Docker images successfuly pulled"
